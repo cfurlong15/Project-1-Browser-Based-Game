9a483509011fbd63ffcf68bb4abf4c14eb2eb054
Conor Furlong <cfurlong15@gmail.com>
2023-11-03 03:01:09 -0400

    finish project and README

---
+++ README.md
@@ -0,0 +1,41 @@
+# Project 1: Browser-based Game
+
+# Concentration
+![](./assets/Screen%20Shot%202023-11-03%20at%202.17.49%20AM.png)
+*The Concentration game board before you begin*
+
+![](./assets/Screen%20Shot%202023-11-03%20at%202.28.58%20AM.png)
+*An example of the board after a successful round*
+
+![](./assets/Screen%20Shot%202023-11-03%20at%202.45.15%20AM.png)
+*An example of the board after an unsuccessful round*
+
+With Concentration, a player is faced with a deck of cards, with each individual card placed face-down. The deck of 16 cards contains 8 matching pairs, all of which are shuffled randomly across the board. The goal of the game is to guess, but also hone your memory, to correctly match each pair. 
+
+## Technologies Used
+This game was developed with three key programming languages:
+- HTML
+- CSS
+- JavaScript
+
+## Getting Started 
+To play Concentration, follow the link below, but be sure to read up on how to play first! 
+
+Concentration: https://cfurlong15.github.io/Project-1-Browser-Based-Game/ 
+
+### How to Play
+Click "Start" to shuffle the deck, then click on a card to flip it over and reveal its face, which in this case will be a color. Keep in mind, though, that you have a limited number of attempts to complete the grid. Every two card flips will register as an attempt, and if you successfully flip each pair within the specified limit, you win. Fail to do so, and the game ends. Win or lose, you always have the option to "Reset" the board, and "Start" again.
+
+## Future Development
+Although designed and functioning, this game is still in its early stages of development. Moving forward, I will continue to implement a number of changes, including: 
+
+Appearance
+- A larger game board containing a larger deck of cards.
+- Images to further identify card faces.
+- An animated flip, so that with each click, the card reveal has more style.
+
+Functionality
+- A point system, so those who are able to win in fewer attempts are rewarded accordingly.
+- A timer to emphasize the stakes and require the player to work more quickly and carefully. 
+
+### Thanks for playing!
--- js/app.js
+++ js/app.js
@@ -1,30 +1,28 @@
-/*----- constants -----*/
-// initial data states
-//animation names
-//image assets paths
-
-
-
 /*----- state variables -----*/
-//state is the data that will change while the game is running
 
+let num1;
+let num2;
+let class1;
+let class2;
+let cardId;
+let firstColor;
 
 
 /*----- cached elements -----*/
 
-
+const start = document.getElementById("start");
 const cardEls = document.querySelectorAll("#grid > div");
+const youWin = document.getElementById("you-win");
+const youLose = document.getElementById("you-lose");
+const resetBtnEl = document.getElementById("reset");
+let cards = document.getElementById("grid");
+let attemptsRemaining = document.querySelector("#remaining");
 
 
+/*----- shuffle functions -----*/
 
-
-
-
-/*----- functions -----*/
 // Source: https://stackoverflow.com/questions/7070054/javascript-shuffle-html-list-element-order
 
-let cards = document.getElementById("grid"),
-start = document.getElementById("start");
 function shuffle(items) {
     let cached = items.slice(0), temp, i = cached.length, rand;
     while(--i) {
@@ -46,130 +44,108 @@ function shuffleNodes() {
 start.onclick = shuffleNodes;
 
 
+/*----- function flipCard state variables -----*/
 
-
-
-const firstPair = document.querySelectorAll(".blue")
-const secondPair = document.querySelectorAll(".second-pair")
-
-let attemptsRemaining = document.querySelector("#remaining");
-const youWin = document.getElementById("you-win");
-const youLose = document.getElementById("you-lose");
-
-let num1;
-let num2;
-let class1;
-let class2;
-let flip = 0
 attemptsRemaining.innerHTML = 20;
-let firstColor;
+let flip = 0
 let win = 0;
-let cardId;
+
+
+/*----- function flipCard -----*/
+
 function flipCard(evt) {
     let cardColor = ""
     flip++
-    const card = evt.target
-    console.log(card)
-    
-   // console.log(card.getAttribute("class"))
+    const card = evt.target;
+    console.log(card);
     let cardClass = card.getAttribute("class");
-    //console.log(class1)
     if(cardClass === "first-pair" && !card.classList.contains("blue")) {
-         cardColor = "blue"
-         cardId = card.id
-         card.classList.add(cardColor)
+         cardColor = "blue";
+         cardId = card.id;
+         card.classList.add(cardColor);
     } else if(cardClass === "second-pair" && !card.classList.contains("green")) {
-        cardColor = "green"
-        cardId = card.id
-        card.classList.add(cardColor)
+        cardColor = "green";
+        cardId = card.id;
+        card.classList.add(cardColor);
     } else if(cardClass === "third-pair" && !card.classList.contains("yellow")) {
-        cardColor = "yellow"
-        cardId = card.id
-        card.classList.add(cardColor)
+        cardColor = "yellow";
+        cardId = card.id;
+        card.classList.add(cardColor);
     } else if(cardClass === "fourth-pair" && !card.classList.contains("orange")) {
-        cardColor = "orange"
-        cardId = card.id
-        card.classList.add(cardColor)
+        cardColor = "orange";
+        cardId = card.id;
+        card.classList.add(cardColor);
     } else if(cardClass === "fifth-pair" && !card.classList.contains("pink")) {
-        cardColor = "pink"
-        cardId = card.id
-        card.classList.add(cardColor)
+        cardColor = "pink";
+        cardId = card.id;
+        card.classList.add(cardColor);
     } else if(cardClass === "sixth-pair" && !card.classList.contains("purple")) {
-        cardColor = "purple"
-        cardId = card.id
-        card.classList.add(cardColor)
+        cardColor = "purple";
+        cardId = card.id;
+        card.classList.add(cardColor);
     } else if(cardClass === "seventh-pair" && !card.classList.contains("red")) {
-        cardColor = "red"
-        cardId = card.id
-        card.classList.add(cardColor)
-    } else if(cardClass === "eighth-pair" && !card.classList.contains("black")) {
-        cardColor = "black"
-        cardId = card.id
-        card.classList.add(cardColor)
+        cardColor = "red";
+        cardId = card.id;
+        card.classList.add(cardColor);
+    } else if(cardClass === "eighth-pair" && !card.classList.contains("white")) {
+        cardColor = "white";
+        cardId = card.id;
+        card.classList.add(cardColor);
     } 
     if(flip === 1) {
     attemptsRemaining.innerHTML--
-        num1=card
-        class1=cardClass
-        firstColor=cardColor
-        
+        num1=card;
+        class1=cardClass;
+        firstColor=cardColor;
      } else if(flip === 2) { 
-        num2 = card
-        class2 = cardClass
+        num2 = card;
+        class2 = cardClass;
     } else if(flip === 2 && cardId === card.id) {
-        flip=1
+        flip=1;
     }
-
-    
-console.log(attemptsRemaining.innerHTML)
-    //console.log(num1, num2)
-if (class1 === class2 && flip === 2) {
-       console.log("same")
-        flip = 0
+    console.log(attemptsRemaining.innerHTML)
+    if (class1 === class2 && flip === 2) {
+       console.log("same");
+        flip = 0;
         win++
     }else if (flip === 2 && class1 !== class2) {
-        console.log("different")
-        num1.classList.remove(firstColor)
-        num2.classList.remove(cardColor)
-       
+        console.log("different");
+        num1.classList.remove(firstColor);
+        num2.classList.remove(cardColor);
         flip = 0;
     }
-
- if (win === 8) {
-   // youWin.append()
-    youWin.innerHTML = "You Win!"
-     console.log("You Win!")
- }
-
-if (attemptsRemaining.innerHTML < 1) {
-    //youLose.append()
-    youLose.innerHTML = "You Lose! Try Again!"
-    console.log("GAME OVER")
-}
+    if (win === 8) {
+        youWin.innerHTML = "You Win!";
+    }
+    if (attemptsRemaining.innerHTML < 1) {
+        youLose.innerHTML = "You Lose! Try Again!"
+    }
 }
 
-cardEls.forEach(function(card) {
-    card.addEventListener("click", flipCard)
-})
 
-
-const resetBtnEl = document.getElementById("reset");
+/*----- function reset -----*/
 
 function reset(evt) {
     if (evt.target === resetBtnEl) {
-        flip = 0
-        attemptsRemaining.innerHTML = 20
+        flip = 0;
+        attemptsRemaining.innerHTML = 20;
         cardEls.forEach(function(card) {
-            card.classList.remove("blue", "green", "yellow", "orange", "pink", "purple", "red", "black")
-            win = 0
-            youWin.innerHTML = ""
-            youLose.innerHTML = ""
-            
+            card.classList.remove("blue", "green", "yellow", "orange", "pink", "purple", "red", "white");
+            win = 0;
+            youWin.innerHTML = "";
+            youLose.innerHTML = "";
         })
-        
     }
 }
-resetBtnEl.addEventListener("click", reset)
+
+
+/*----- event listeners -----*/
+
+cardEls.forEach(function(card) {
+    card.addEventListener("click", flipCard);
+})
+
+resetBtnEl.addEventListener("click", reset);
 
 
 
